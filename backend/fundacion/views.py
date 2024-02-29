7672f9e01a72f214f525f177c30bd05a3554c3e0
Stiven <69906848+BrayanSRojasG14@users.noreply.github.com>
2024-02-28 21:13:21 -0500

    4.0.2 Errores en views del aplicativo

--- backend/fundacion/views.py
+++ backend/fundacion/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import F
+from django.urls import reverse
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        "latest_question_list":latest_question_list
+        }
+    return render(request, "fundacion/index.html", context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "fundacion/detail.html", {"question":question})
 
 def results(request, question_id):
@@ -20,9 +21,20 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "fundacion.detail.html",
+            {
+                "question":question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
     return HttpResponse("You're voting on question %s." %question_id)
 
 
 def login(request):
     return HttpResponse("Iniciar sesion.")
-
