32f36583c2a0c746d2b9e065dc2239ef1a82e0b6
Stiven <69906848+BrayanSRojasG14@users.noreply.github.com>
2024-03-14 23:23:25 -0500

    4.1.2 Change of templates to generic views

--- backend/fundacion/views.py
+++ backend/fundacion/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list":latest_question_list
-        }
-    return render(request, "fundacion/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "fundacion/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "fundacion/detail.html", {"question":question})
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "fundacion/detail.html"
+    
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "fundacion/results.html"
+    
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -27,14 +30,17 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(
             request,
-            "fundacion.detail.html",
+            "fundacion/detail.html",
             {
                 "question":question,
                 "error_message": "You didn't select a choice.",
             },
         )
-    return HttpResponse("You're voting on question %s." %question_id)
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("fundacion:results", args=(question.id,)))
 
 
 def login(request):
-    return HttpResponse("Iniciar sesion.")
+    return render("Iniciar sesion.")
